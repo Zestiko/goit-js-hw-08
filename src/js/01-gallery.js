77a3cf2139023a028fbeb3df69f367a46f531240
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-10-25 22:33:44 +0300

    finish 1st task

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+const { gallery} = refs;
+const galleryItemsRefs = galleryItems.map(({preview, original, description}) => `
+    <a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}" 
+        alt="${description}"
+        data-source="${original}"
+    />
+    </a>
+ `)
+    .join('');
+// console.log(galleryItemsRefs)
+gallery.innerHTML = galleryItemsRefs;
+
+let galleryNew = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
